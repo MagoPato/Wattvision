24db583cf27e822cd4821b385d59d6c1317611a6
David Patricio Escamilla Marquez <lego172002@gmail.com>
2024-11-21 00:22:05 -0600

    Update header.php
    se agrego unos css que faltaba de declara en general y también se agrego un script para que cada ves entres a una pagina siempre este arriba

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,10 @@
-<link rel="icon" href="../img/icono.png" type="image/png"> <!-- Ruta al favicon -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="../css/cursor.css">
+ <script>
+     if ('scrollRestoration' in history) {
+         history.scrollRestoration = 'manual';
+     }
+ </script>
+
+ <link rel="icon" href="../img/icono.png" type="image/png"> <!-- Ruta al favicon -->
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+ <link rel="stylesheet" href="../css/cursor.css">
+ <link rel="stylesheet" href="../css/animacion.css">
